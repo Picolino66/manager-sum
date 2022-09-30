e0a25f2e272379ef971dc6df4b998413159f1d71
Picolino66 <isaiasbd@hotmail.com>
2022-09-30 01:18:44 -0300

    android

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var device: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  ngOnInit(): void {
+      document.addEventListener("deviceready", () => alert(device.platform))
+  }
   title = 'sum';
 }
